593a7103b647ac4b85cfaf5c9b83714f7293d532
Brooks <Skoorb412785@yahoo.com>
2020-03-06 14:30:41 -0500

    rendered a component  in main

--- src/titles/Titles.js
+++ src/titles/Titles.js
@@ -1,21 +1,46 @@
 import React, {Component} from 'react';
+import { Route, Link, Redirect } from "react-router-dom";
 import axios from 'axios';
 
 class Titles extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: null
+        }
+    }
     componentDidMount () {
-        const ax = axios.create ({
-            BaseURL: 'http://localhost:5000/'
-        });
-        const get = ax.get('/');
-        console.log(get)
+       if (!this.state.data) {
+           fetch('http://localhost:5000/')
+           .then(res => res.json())
+           .then(res => {
+               this.setState({ data: res})
+               console.log(res)
+           })
+       }
     }
     render() {
-        return (
-            <div>
-
-            </div>
-        )
+        if (this.state.data) {
+            let list = this.state.data.map(item => {
+                return (
+                    <div className="eachReturn" key={item.title}>
+                        <Link className="reviewLink" to={"/review/" + item.variety}>
+                            <p>{item.title}</p>
+                            <p>{item.taster_twitter_handle}</p>
+                        </Link>
+                    </div>
+                );
+            });
+            return <div className="container">{list}</div>;
+        } else {
+            return (
+                <div>
+                    <h2>Loading...</h2>
+                </div>
+            )
+        }
     }
 }
+
 
 export default Titles;
